3cdd07478236e6641ec9355be54689e54fe29532
ahmadalbourhan <127745237+ahmadalbourhan@users.noreply.github.com>
2024-04-05 15:48:38 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,96 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import {
+  getDatabase,
+  ref,
+  push,
+  onValue,
+  remove,
+} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+
+const appSettings = {
+  databaseURL:
+    "https://playground-65f1d-default-rtdb.europe-west1.firebasedatabase.app/",
+};
+
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const shoppingListInDB = ref(database, "shoppingList");
+
+const inputFieldEl = document.getElementById("input-field");
+const addButtonEl = document.getElementById("add-button");
+const shoppingListEl = document.getElementById("shopping-list");
+
+addButtonEl.addEventListener("click", function () {
+  let inputValue = inputFieldEl.value.trim().toLowerCase(); // Convert input to lowercase
+
+  // Check if input value is empty
+  if (inputValue === "") {
+    pop("Please enter a valid item.");
+    return;
+  }
+
+  // Query the database to check if the item already exists
+  let itemExists = false;
+  onValue(shoppingListInDB, function (snapshot) {
+    if (snapshot.exists()) {
+      Object.values(snapshot.val()).forEach((value) => {
+        if (value.toLowerCase() === inputValue) {
+          // Convert existing item to lowercase for comparison
+          itemExists = true;
+        }
+      });
+    }
+  });
+
+  // If the item does not exist, push it to the database
+  if (!itemExists) {
+    push(shoppingListInDB, inputFieldEl.value); // Push the original input value
+  } else {
+    alert("This item is already in the list.");
+  }
+
+  clearInputFieldEl();
+});
+
+onValue(shoppingListInDB, function (snapshot) {
+  if (snapshot.exists()) {
+    let itemsArray = Object.entries(snapshot.val());
+
+    clearShoppingListEl();
+
+    for (let i = 0; i < itemsArray.length; i++) {
+      let currentItem = itemsArray[i];
+      let currentItemID = currentItem[0];
+      let currentItemValue = currentItem[1];
+
+      appendItemToShoppingListEl(currentItem);
+    }
+  } else {
+    shoppingListEl.innerHTML = "No items here... yet";
+  }
+});
+
+function clearShoppingListEl() {
+  shoppingListEl.innerHTML = "";
+}
+
+function clearInputFieldEl() {
+  inputFieldEl.value = "";
+}
+
+function appendItemToShoppingListEl(item) {
+  let itemID = item[0];
+  let itemValue = item[1];
+
+  let newEl = document.createElement("li");
+
+  newEl.textContent = itemValue;
+
+  newEl.addEventListener("click", function () {
+    let exactLocationOfItemInDB = ref(database, `shoppingList/${itemID}`);
+
+    remove(exactLocationOfItemInDB);
+  });
+
+  shoppingListEl.append(newEl);
+}
